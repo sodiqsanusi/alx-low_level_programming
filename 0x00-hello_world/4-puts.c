dba5aded86d9ecef595829d97c9fb153402d4b28
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2023-09-07 22:00:23 -0700

    Against time

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void) /*This is the main function */
+/*The beginnig of the main*/
+{
+	/*Creation of puts function*/
+	puts("\"Programming is like building a multilingual puzzle");
+	/*This indicates the end of program*/
+	return (0);
+}
