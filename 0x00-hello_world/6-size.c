87243e9428203053c2cd06ddde8c6911dd59f79f
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2023-09-08 17:08:02 -0700

    Bug, Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
- * Return: always 0 (Success)
  *
+ * Description: using sizeof to print the size of various types.
  *
- */
+ * Return: Always 0 (Succesful)
+*/
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	/* sizeof evaluates the size of a variable */
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of an double: %lu byte(s)\n", sizeof(double));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+
 	return (0);
+
 }
