d27cda2c1d92f7a29c68d14e1ff6c571293d2383
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2023-09-09 19:39:44 -0700

    Tasks 6-9
    Done with all the mandatory tasks

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Entry point for the C program
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 122; n > 96; n--)
+	{
+		putchar(n);
+	}
+	putchar(10);
+	return (0);
+}
+
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - Entry point for the C program
+*
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	for (n = 97; n < 103; n++)
+	{
+		putchar(n);
+	}
+	putchar(10);
+	return (0);
+}
+
