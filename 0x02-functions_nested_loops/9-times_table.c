7c8efb06018d885f75b94dbf0b9973f7cd6a51cd
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2023-09-12 11:32:50 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* times_table - Prints the timetable up till 9 times
+*
+* Return: Void
+*/
+
+void times_table(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		int i = 0;
+
+		while (i < 9)
+		{
+			_putchar(i * n);
+			_putchar(44);
+			_putchar(32);
+		}
+		_putchar(i);
+		_putchar(10);
+	}
+	return (0);
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,4 +7,5 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 int add(int, int);
+void times_table(void);
 
