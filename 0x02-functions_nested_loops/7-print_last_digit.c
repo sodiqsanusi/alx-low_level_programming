d040003f9527e6fc09e95ec94aab43c05d7978d6
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2023-09-12 09:24:29 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* print_last_digit - Prints the last digit of a given integer
+* @n: The integer parameter from where the last digit is taken
+*
+* Return: The last digit of the integer given
+*/
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
+
