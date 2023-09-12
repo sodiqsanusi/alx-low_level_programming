f9dcd04d848ccb70e53246a6ff6c472393d0747e
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2023-09-12 04:26:49 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+* _putchar - Function declaration for the _putchar function in another file
+* @c: Character to be written to STDOUT
+*
+* Return: Integer that prints to the terminal
+*/
+int _putchar(char c);
+
+/**
+* main - Entry point for the C program
+*
+* Return: Always 0 (sucess)
+*/
+int main(void)
+{
+	_putchar(95);
+	_putchar(112);
+	_putchar(117);
+	_putchar(116);
+	_putchar(99);
+	_putchar(104);
+	_putchar(97);
+	_putchar(114);
+	_putchar(10);
+	return (0);
+}
+
