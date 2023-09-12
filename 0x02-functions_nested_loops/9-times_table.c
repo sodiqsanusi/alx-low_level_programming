9bd84de6c3bfe94daccf7fc2ba0ea878015d21a3
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2023-09-12 16:16:40 -0700

    Last task hopefully

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,24 +5,32 @@
 *
 * Return: Void
 */
-
 void times_table(void)
 {
-	int n;
+	int num, mult, prod;
 
-	for (n = 0; n < 10; n++)
+	for (num = 0; num <= 9; ++num)
 	{
-		int i = 0;
-
-		while (i < 9)
+		_putchar(48);
+		for (mult = 1; mult <= 9; ++mult)
 		{
-			_putchar(i * n);
-			_putchar(44);
-			_putchar(32);
-			i++;
+			_putchar(',');
+			_putchar(' ');
+
+			prod = num * mult;
+
+			/*
+			 * put space if product is a single number
+			 * place the first digit if its two numbers
+			*/
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + 48); /*get the first digit*/
+
+			_putchar((prod % 10) + 48); /*get the second digit*/
 		}
-		_putchar(i);
-		_putchar(10);
+		_putchar('\n');
 	}
 }
 
