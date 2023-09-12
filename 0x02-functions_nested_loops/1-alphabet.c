6c81894128bde3e661f8f46ba51156d9b0c2507c
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2023-09-12 05:41:41 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - The entry point for the C program
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		_putchar(c);
+	}
+	_putchar(10);
+	return (0);
+}
+
