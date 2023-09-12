14a8b9133e715a951b53584ebb79ff666b5bded1
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2023-09-12 11:17:16 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* add - Gives the result of two integers added together
+* @int1: First integer parameter
+* @int2: Second integer paramter
+*
+* Return: An integer result of the addition of the two parameters
+*/
+int add(int int1, int int2)
+{
+	return (int1 + int2);
+}
+
