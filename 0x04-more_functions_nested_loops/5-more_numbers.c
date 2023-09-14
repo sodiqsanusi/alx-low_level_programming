698e5f7bc3f88146893a32a516ee2d21f1b1165e
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2023-09-14 07:43:49 -0700

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* more_numbers - Prints the numbers 0 - 14, ten times
+*
+* Return: Nothing
+*/
+void more_numbers(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		int p = 0, d;
+
+		while (p < 15)
+		{
+			d = p;
+			if (p / 10)
+			{
+				_putchar(48 + 1);
+				d = p % 10;
+			}
+			_putchar(48 + d);
+		}
+		_putchar(10);
+	}
+}
+
