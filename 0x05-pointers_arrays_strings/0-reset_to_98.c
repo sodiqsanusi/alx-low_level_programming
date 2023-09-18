331abeb676d6a93ff1cf92c10c8ddc2f39e904ca
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2023-09-18 16:53:43 -0700

    Initial commit, C - Pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* reset_to_98 - Changes a integer pointer value to 98
+* @n: The pointer from which we will update a value
+*
+* Return: Nothing (void)
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
