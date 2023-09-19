6af649df81eafc49c121e5832519919c602693d9
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2023-09-18 19:22:16 -0700

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* puts2 - Prints the odd characters in a string on the terminal
+* @str: String to be used
+*
+* Return: Nothing (void)
+*/
+void puts2(char *str)
+{
+	int num_check;
+
+	for (num_check = 1; *str != '\0'; str++)
+	{
+		if (num_check % 2)
+			_putchar(*str);
+	}
+	_putchar(10);
+}
+
