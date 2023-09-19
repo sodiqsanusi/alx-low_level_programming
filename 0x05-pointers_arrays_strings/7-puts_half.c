c6f75e3ecead406fdf3c47e1078b97e9b1f82277
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2023-09-18 20:18:11 -0700

    Task 7, hopefully

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* puts_half - Prints half of a string
+* @str: The string
+*
+* Return: Nothing (void)
+*/
+void puts_half(char *str)
+{
+	int length, half;
+
+	for (length = 0; str[length] != '\0'; length++)
+		;
+	if (length % 2 == 0)
+	{
+		for (half = length / 2; str[half] != '\0'; half++)
+			_putchar(str[half - 1]);
+	}
+	else
+	{
+		for (half = (length / 2) + 1; str[half] != '\0'; half++)
+			_putchar(str[half - 1]);
+	}
+	_putchar(10);
+}
+
