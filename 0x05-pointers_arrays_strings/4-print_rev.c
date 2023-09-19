f4e1ca01dd34cebcd043200676ae9d6b56d6e370
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2023-09-18 18:48:57 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* print_rev - Prints a string in reverse
+* @s: String to be printed
+*
+* Return: Nothing (void)
+*/
+void print_rev(char *s)
+{
+	int string_length, rev_int;
+
+	for (string_length = 0; *s != '\0'; s++)
+		string_length++;
+	for (rev_int = string_length; rev_length > 0; rev_length--)
+	{
+		s--;
+		_putchar(*s);
+	}
+	_putchar(10);
+}
+
