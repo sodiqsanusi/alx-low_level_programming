6505f49b33e1f651f628b9758e6398cac2779731
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2023-09-21 10:07:31 -0700

    Skipped to task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* leet - Encodes a string in the 1337 cypher format
+* @str: The string to be modified (encoded)
+*
+* Return: A pointer to the modified string
+*/
+char *leet(char *str)
+{
+	int lilac;
+	int bad_lower[] = {97, 101, 111, 108, 116};
+	int bad_upper[] = {65, 69, 79, 76, 84};
+	int good[] = {52, 51, 48, 49, 55}
+
+	while (*str)
+	{
+		for (lilac = 0; lilac < 5; lilac++)
+		{
+			if ((*str == bad_lower[lilac]) || (*str == bad_upper[lilac]))
+			{
+				*str = good[lilac];
+				break;
+			}
+		}
+		str++;
+	}
+
+	return (str);
+}
+
