158c0e145b28edc252a53249e898a8b9f455577d
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2023-09-21 08:46:59 -0700

    Task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* reverse_array - reverses the content of an array of integers
+* @a: The array of integers
+* @n: The length of the array
+*
+* Return: Nothing (void)
+*/
+void reverse_array(int *a, int n)
+{
+	int half_num;
+
+	for (half_num = 0; half_num != n / 2; half_num++)
+	{
+		int temp = a[half_num];
+
+		a[half_num] = a[n - half_num - 1];
+		a[n - half_num - 1] = temp;
+	}
+}
+
