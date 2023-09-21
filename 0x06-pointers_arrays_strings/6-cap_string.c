57a8a4b4fac18e2be959fd3984486b547cc4ae14
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2023-09-21 09:49:34 -0700

    Frustrated and tired

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,22 +13,22 @@ char *cap_string(char *str)
 
 	while (*str_cpy)
 	{
-		if ((*str_cpy == 32) || (*str_cpy == 9) || (*str_cpy == 10)
-			|| (*str_cpy == 44) || (*str_cpy == 59) || (*str_cpy == 46)
-			|| (*str_cpy == 33) || (*str_cpy == 63) || (*str_cpy == 34)
-			|| (*str_cpy == 40) || (*str_cpy == 41) || (*str_cpy == 123)
-			|| (*str_cpy == 125))
+		if ((*str_cpy == ' ') || (*str_cpy == '\t') || (*str_cpy == '\n')
+			|| (*str_cpy == ',') || (*str_cpy == ';') || (*str_cpy == '.')
+			|| (*str_cpy == '!') || (*str_cpy == '?') || (*str_cpy == '"')
+			|| (*str_cpy == '(') || (*str_cpy == ')') || (*str_cpy == '{')
+			|| (*str_cpy == '}'))
 		{
 			flag = 1;
 			str_cpy++;
 			continue;
 		}
-		if ((flag == 1) && (*str_cpy > 96) && (*str_cpy < 123))
+		if ((flag == 1) && (*str_cpy >= 'a') && (*str_cpy <= 'z'))
 		{
 			*str_cpy -= 32;
 			flag = 0;
 		}
-		if ((flag == 0) && (*str_cpy > 64) && (*str_cpy < 91))
+		if ((flag == 0) && (*str_cpy >= 'A') && (*str_cpy <= 'Z'))
 			*str_cpy += 32;
 		str_cpy++;
 	}
