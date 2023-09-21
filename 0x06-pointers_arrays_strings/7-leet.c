383e18497a3dbff4efa6c8d05ce6b468b4c14127
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2023-09-21 10:13:01 -0700

    Bugs everywhere, and my tears too

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,10 +8,11 @@
 */
 char *leet(char *str)
 {
+	char *original_str = str;
 	int lilac;
 	int bad_lower[] = {97, 101, 111, 108, 116};
 	int bad_upper[] = {65, 69, 79, 76, 84};
-	int good[] = {52, 51, 48, 49, 55}
+	int good[] = {52, 51, 48, 49, 55};
 
 	while (*str)
 	{
@@ -26,6 +27,6 @@ char *leet(char *str)
 		str++;
 	}
 
-	return (str);
+	return (original_str);
 }
 
