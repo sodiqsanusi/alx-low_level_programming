282f51d76e8d29a8d3bbc141451e5659ea4bf510
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2023-09-28 04:44:40 -0700

    Task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,24 @@
 #include "main.h"
 
 /**
+* checker - The main recursive function that checks for prime
+* @tester: a number that is used to divide against the possible prime as a test
+* @n: The possible prime
+*
+* Return: 1 if input "n" is a prime, 0 otherwise
+*/
+int checker(int tester, int n)
+{
+	if ((n % tester == 0) || n < 2)
+		return (0);
+	else if (tester + 1 == n)
+		return (1);
+	else if (n > tester)
+		return (checker(tester + 1, n));
+	return (1);
+}
+
+/**
 * is_prime_number - Checks if input is a prime number
 * @n: The number to be checked
 *
@@ -8,5 +26,6 @@
 */
 int is_prime_number(int n)
 {
+	return (checker(2, n));
 }
 
