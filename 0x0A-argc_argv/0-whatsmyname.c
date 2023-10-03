fb65bb8e9b35df914bdd0e7be458036d02c1a282
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2023-10-02 20:58:05 -0700

    Initial commit, Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - Entry point for the C program
+* @argc: Argument count from the terminal
+* @argv: Array of string containing the terminal commands
+*
+* Return: Nothing (void)
+*/
+void main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+}
+
