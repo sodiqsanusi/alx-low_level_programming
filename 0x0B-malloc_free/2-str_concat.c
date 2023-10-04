b27b7f2b0225d2925b39c42ac7b3fb5f68e99be7
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2023-10-03 19:38:02 -0700

    Task 2 bug, trial 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -22,13 +22,13 @@ char *str_concat(char *s1, char *s2)
 	if (c_string == NULL)
 		return (NULL);
 
-	while (*s1 && (s1 != NULL))
+	while ((s1 != NULL) && *s1)
 	{
 		c_string[iterator] = *s1;
 		iterator++;
 		s1++;
 	}
-	while (*s2 && (s2 != NULL))
+	while ((s2 != NULL) && *s2)
 	{
 		c_string[iterator] = *s2;
 		iterator++;
