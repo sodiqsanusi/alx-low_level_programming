91241891e3f9a74fbc58f9fdc1f862b9718e71c0
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2023-10-04 14:42:06 -0700

    Task 3, a checker is failing lol

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,7 +15,10 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	grid = (int **)malloc(height * sizeof(int *));
 	if (grid == NULL)
+	{
+		free(grid);
 		return (NULL);
+	}
 
 	for (i = 0; i < height; i++)
 	{
