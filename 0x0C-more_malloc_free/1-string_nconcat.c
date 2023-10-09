dee4354d8d9a6b05e80cbcae30d3eebbbf88485d
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2023-10-09 01:17:01 -0700

    Lows, deep lows for me

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,35 +10,40 @@
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *new_string;
-	int s1_length = 0;
-	unsigned int count = 0;
+	unsigned int l1, l2, i, j;
+	char *str;
 
-	if (s1 != NULL)
-	{
-		for (s1_length = 0; s1[s1_length]; s1_length++)
-			;
-	}
+	/*treat NULL as empty string*/
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-	new_string = malloc(sizeof(char) * (s1_length + n + 1));
-	if (new_string == NULL)
+	l1 = l2 = 0;
+	/*length of both s1 & s2 respectively*/
+	while (s1[l1] != '\0')
+		l1++;
+	while (s2[l2] != '\0')
+		l2++;
+
+	/*set n to length of s2*/
+	if (n >= l2)
+		n = l2;
+
+	str = (char *) malloc((l1 + n + 1) * sizeof(char));
+	if (str == NULL)
 		return (NULL);
 
-	while (s1 && *s1)
+	/*add s1 to str*/
+	for (i = 0; s1[i] != '\0'; i++)
+		str[i] = s1[i];
+	/*add s2 n bytes to str*/
+	for (j = 0; j < n && s2[j] != '\0'; j++)
 	{
-		*new_string = *s1;
-		new_string++;
-		s1++;
+		str[i] = s2[j];
+		i++;
 	}
-	while (s2 && *s2 && (count <= n))
-	{
-		*new_string = *s2;
-		new_string++;
-		s2++;
-		n++;
-	}
-	*new_string = '\0';
-
-	return (new_string);
+	str[i] = '\0';
+	return (str);
 }
 
