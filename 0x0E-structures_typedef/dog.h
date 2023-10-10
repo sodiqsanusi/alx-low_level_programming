3c20d592debd04e1d99718713b5a864716b20057
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2023-10-10 15:48:37 -0700

    Initial commit, Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - Holds the properties for a dog
+* @name: The dog's name
+* @age: The dog's age
+* @owner: The dog's owner name
+*/
+struct dog
+{
+	char *name;
+	int age;
+	char *owner;
+}
+
+#endif
+
