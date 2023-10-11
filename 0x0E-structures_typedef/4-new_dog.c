4bc80bd6417f25c58add473a5556ede79caf11f7
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2023-10-10 17:36:14 -0700

    Task 4 bug

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -30,12 +30,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	for (i = 0; i < name_len; i++)
 		doggy->name[i] = name[i];
+	doggy->name[i] = '\0';
 
 	doggy->owner = malloc(sizeof(doggy->owner) * owner_len);
 	if (doggy == NULL)
 		return (NULL);
 	for (i = 0; i < owner_len; i++)
 		doggy->owner[i] = owner[i];
+	doggy->owner[i] = '\0';
 
 	return (doggy);
 }
