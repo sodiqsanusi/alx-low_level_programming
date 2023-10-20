c39a529b7f2e4b1d70530f9c05c488b7509417f0
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2023-10-19 19:08:33 -0700

    Task 100, advanced

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+* beforeMain - A function that runs before the main function
+*
+* Return: "void" Nothing
+*/
+void beforeMain(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
