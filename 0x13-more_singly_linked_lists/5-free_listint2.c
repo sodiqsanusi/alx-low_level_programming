e2c0d8ed7236c188528cc2378a2c961cfa4a3649
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2023-10-23 13:11:58 -0700

    Task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+* free_listint2 - Frees the memory allocated to a singly-linked list
+* @head: A pointer to a pointer that points to the head of the list
+*
+* Return: "void" Nothing
+*/
+void free_listint2(listint_t **head)
+{
+	nodePtr temp = *head;
+
+	while (temp)
+	{
+		*head = temp->next;
+		free(temp);
+		temp = *head;
+	}
+}
+
