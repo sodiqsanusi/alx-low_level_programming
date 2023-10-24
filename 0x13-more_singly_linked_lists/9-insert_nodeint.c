35c62add1d5364b826fc9eed39577692ffe34a15
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2023-10-24 06:08:41 -0700

    Yea, that was the mistake

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -39,6 +39,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	newNode->next = currentNode->next;
 	currentNode->next = newNode;
-	return (*head);
+	return (newNode);
 }
 
