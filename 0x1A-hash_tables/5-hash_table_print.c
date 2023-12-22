4b9b666183ce45ad0703ac580cef0a885add44f6
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2023-12-22 01:40:27 -0800

    Task 5: Print a hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+* hash_table_print - print the hash table
+* @ht: the hash table
+*
+* Return: "void" Nothing
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i, j;
+	hash_node_t *tmp;
+
+	i = j = 0;
+	if (!ht)
+		return;
+	putchar('{');
+	for (i = j = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			tmp = ht->array[i];
+			while (tmp)
+			{
+				printf("%s'%s': '%s'", j == 0 ? "" : ", ", tmp->key, tmp->value), j++;
+				tmp = tmp->next;
+			}
+		}
+	}
+	printf("}\n");
+}
+
