8222005b0ad5484d54081a23bdf88b2f58c542e9
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2024-04-09 17:19:38 -0700

    Task 1 bug: If array pointer is NULL?

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -38,6 +38,9 @@ int binary_search(int *array, size_t size, int value)
 	size_t high = size - 1;
 	size_t mid;
 
+	if (array == NULL)
+		return (-1);
+
 	while (low <= high)
 	{
 		mid = (int)low + (high - low) / 2;
