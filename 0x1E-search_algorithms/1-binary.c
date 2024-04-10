910af35d63c9cf9c8ff4cd8a3ce1ed1beedd5179
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2024-04-09 17:13:37 -0700

    Task 1: First trial in implementing the binary search algorithm

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,6 +1,30 @@
 #include "search_algos.h"
 
 /**
+* print_array - Prints a given array following some specifications
+* @array: "int *" Array from which some elements will be printed
+* @low: "size_t" The lower boundary to print elements from array
+* @high: "size_t" The higher boundary to stop printing elements
+*
+* Return: "void" Nothing
+*/
+void print_array(int *array, size_t low, size_t high)
+{
+	size_t i;
+
+	printf("Searching in array: ");
+	for (i = low; i <= high; i++)
+	{
+		if (i == high)
+		{
+			printf("%d\n", array[i]);
+			continue;
+		}
+		printf("%d, ", array[i]);
+	}
+}
+
+/**
 * binary_search - Implement a binary search algorithm to get an element
 * @array: "int *" Array of integers being searched
 * @size: "size_t" The number of elements in the array
@@ -10,5 +34,21 @@
 */
 int binary_search(int *array, size_t size, int value)
 {
+	size_t low = 0;
+	size_t high = size - 1;
+	size_t mid;
+
+	while (low <= high)
+	{
+		mid = (int)low + (high - low) / 2;
+		print_array(array, low, high);
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+			low = mid + 1;
+		else
+			high = mid - 1;
+	}
+
 	return (-1);
 }
